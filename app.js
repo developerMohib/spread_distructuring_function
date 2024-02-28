453353069d80f630318d6bbc829dffafd030173c
Mohib <mohibullahmohim2020@gmail.com>
2024-02-28 12:21:40 +0600

    spread operator something hit me

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('connected')
+console.log('Spreading Operator....');
+
+// spread operator
+
+const max = Math.max(4,2,5,7,11,21,22);
+console.log('The max Number is : ', max);
+
+const maxFromArray = [33, 22,44, 32, 34, 65];
+const maxNum = Math.max(...maxFromArray);
+// console.log( 'anything wrong i know - ', maxNum);
+console.log( 'all right i know - ', maxNum);
